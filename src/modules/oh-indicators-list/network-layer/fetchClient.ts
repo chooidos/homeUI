065252797bef68e29d18af82b0112658d8550f89
Illia Lukashchuk <iluka@softserveinc.com>
2021-07-16 17:25:39 +0300

    added(fetchClients): server-sent event, item states

--- src/modules/oh-indicators-list/network-layer/fetchClient.ts
+++ src/modules/oh-indicators-list/network-layer/fetchClient.ts
@@ -7,3 +7,22 @@ export const fetchAllItems = async () => {
   const response = await axios.get<IOpenhabItem[]>(`${BASE_URL}/rest/items`, {});
   return response.data;
 };
+
+export const sseitems = async ()=> {
+  const events = new EventSource(`${BASE_URL}/rest/events?topics=openhab/items/*/statechanged,openhab/items/*/*/statechanged`);
+  events.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    if (data.type === 'ItemStateChangedEvent') {
+      const [,,name] = data.topic.split('/');
+      const value = JSON.parse(data.payload).value;
+      return {name,value}
+    }
+  }
+}
+
+export const getItemStates = async (name: string, startTime?: string) => {
+  const response = await axios.get(`${BASE_URL}/rest/persistence/items/${name}`, {params:{
+    starttime: startTime,
+  }});
+  return response.data;
+}
